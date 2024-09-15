6d371ce5a811e075b8a1704881967904d2a52903
Awika <awikamaurice@gmail.com>
2024-09-15 15:52:19 +0300

    this is my first commit commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result1 = number1+number2
+result2 = number1-number2
+result3 = number1*number2
+print("Addition of", number1, "and" ,number2 ,"is",result1)
+print("Subtraction of", number1, "and" ,number2 ,"is",result2)
+print("Multiplication of", number1, "and" ,number2 ,"is",result3)
