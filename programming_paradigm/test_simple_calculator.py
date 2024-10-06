3657326b9a1c7ef4eb387cbd44c23428220a03a7
Awika <awikamaurice@gmail.com>
2024-10-06 20:51:20 +0300

    this is my programming paradigm fifth commit

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,20 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        self.assertEqual(self.calc.add(5,10),15)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5,10),-5)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(5,10),50)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10,5),2)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
+
